27733ee29e3c59dcc20c8b4530df9462ace1e122
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2021-10-16 23:58:10 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,41 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">Logo Here</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+       <div class="navbar-nav mr-auto">
+         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+      <router-link to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link to="/cats">Cats</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link to="/dogs">Dogs</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link to="/pet">Pet</router-link>
+      </li>
+      </ul>
     </div>
+    <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+        </div>
+</nav>
+</div>
+    <b-container>
     <router-view/>
+    </b-container>
   </div>
 </template>
 
@@ -19,14 +50,21 @@
 
 #nav {
   padding: 30px;
+  text-align: center;
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
+     font-weight: bold;
+     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color:whitesmoke;
+      background: #42b983;
+      border-radius: .10rem;
+      padding: 5px;
     }
+  }
+  li {
+    padding: 5px;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
+<div>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Adopt a new best friend.</h1>
   </div>
+  <div class="card-deck">
+  <div class="card">
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style>
+h1 {
+  margin-bottom: 15px;
+}
+</style>
